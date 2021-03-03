08f4f1642eccc9367f7a740175f39c317f993399
bqxtt <1486126243@qq.com>
2021-03-03 18:33:08 +0800

    crawler client

--- client/rpc_crawler/client.go
+++ client/rpc_crawler/client.go
@@ -3,12 +3,11 @@ package rpc_crawler
 import (
 	"github.com/ecnuvj/vhoj_common/pkg/common/constants/rpc_config"
 	"github.com/ecnuvj/vhoj_rpc/model/crawlerpb"
-	"github.com/ecnuvj/vhoj_rpc/model/problempb"
 	"google.golang.org/grpc"
 	"log"
 )
 
-var CrawlerServiceClient crawlerpb.ProblemServiceClient
+var CrawlerServiceClient crawlerpb.CrawlerServiceClient
 
 func Init() {
 	conn, err := grpc.Dial(rpc_config.ProblemRpc.GetFullAddress(), grpc.WithInsecure(), grpc.WithBlock())
@@ -16,5 +15,5 @@ func Init() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	log.Println("problem rpc service connect...")
-	ProblemServiceClient = problempb.NewProblemServiceClient(conn)
+	CrawlerServiceClient = crawlerpb.NewCrawlerServiceClient(conn)
 }
